d2c90c9c2ed89b615cfe15ba01cd26728fcba6d3
joseph dillon <joseph.dillon@vercoglobal.com>
2019-06-14 16:11:47 +0100

    initial dashboard before highcharts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>React grid layout test </h1>
+      <div className="layout-container">
+        <nav role="navigation" className="left-nav-container">
+          <div>this is left nav</div>
+          <div>this is left nav</div>
+          <div>this is left nav</div>
+          <div>this is left nav</div>
+          <div>this is left nav</div>
+          <div>this is left nav</div>
+          <div>this is left nav</div>
+          <div>this is left nav</div>
+          <div>this is left nav</div>
+        </nav>
+        <div className="content-container scrollbar">
+          <Dashboard />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Dashboard.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { WidthProvider, Responsive } from "react-grid-layout";
+import _ from "lodash";
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+
+const ffs = [
+  { i: "1", h: 4, w: 5, x: 0, y: 0 },
+  { i: "2", h: 3, w: 3, x: 0, y: 4 },
+  { i: "3", h: 2, w: 4, x: 0, y: 8 }
+];
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      layouts: { lg: ffs },
+      widgets: ffs
+    };
+  }
+
+  onLayoutChange(layout, layouts) {
+    this.setState({ layouts });
+  }
+
+  render() {
+    console.log(this.state.layouts);
+
+    return (
+      <div className="dashboard-wrapper full-height">
+        <div className="dashboard-grid">
+          <ResponsiveReactGridLayout
+            className="layout"
+            cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+            rowHeight={30}
+            useCSSTransforms="true"
+            layouts={this.state.layouts}
+            onLayoutChange={(layout, layouts) =>
+              this.onLayoutChange(layout, layouts)
+            }
+            isDraggable="true"
+            isResizable="true"
+            compactType="vertical"
+          >
+            {this.state.widgets.map(l => {
+              return (
+                <div key={l.i} className="dashboard-widget">
+                  <div className="widget widget-edit">widget cont {l.i}</div>
+                </div>
+              );
+            })}
+          </ResponsiveReactGridLayout>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
